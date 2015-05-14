bc79088723d457fa8dc10309407ed54a52b4af13
t0k3nZz <djsalazar0001@zonemail.clpccd.edu>
2015-05-14 00:56:15 -0700

    Create drand.h

---
+++ drand.h
@@ -0,0 +1,15 @@
+/*
+** drand.h
+** Keith Mehl 5/4/05
+** prototypes for the drand.cpp module
+*/
+#ifndef DRAND_H_INCLUDED
+#define DRAND_H_INCLUDED
+
+void initrand(unsigned long seed);
+double drand( void );
+long lrand( void );
+
+#endif // ndef DRAND_H_INCLUDED
+
+
